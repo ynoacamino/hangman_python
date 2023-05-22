dcdbc1814c8d469a84d9a478cb8fcd867f520629
Diegoo11 <ynoacamino@gmail.com>
2023-05-21 23:17:41 -0500

    primer avanze

---
+++ game.py
@@ -0,0 +1,28 @@
+"""Module for draw."""
+import turtle
+
+
+def new_square(tortuga, width, height):
+    """Funcion creadora de cuadrados apartir de una tortuga previamente creada"""
+    tortuga.forward(width)
+    tortuga.right(90)
+    tortuga.forward(height)
+    tortuga.right(90)
+    tortuga.forward(width)
+    tortuga.right(90)
+    tortuga.forward(height)
+
+
+window = turtle.Screen()
+
+floor = turtle.Turtle()
+floor.hideturtle()
+# floor.speed(70)
+floor.fillcolor("black")
+floor.penup()
+floor.goto(-200, -100)
+floor.pendown()
+new_square(floor, 400, 20)
+
+
+turtle.done()
